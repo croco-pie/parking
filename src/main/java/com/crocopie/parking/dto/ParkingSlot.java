67c82fa072efba82d308d55e72e02eded1a6cb01
crocopie <nikulina.annaa@yandex.ru>
2023-04-16 21:09:10 +0300

    add get available parking slots endpoint

--- src/main/java/com/crocopie/parking/dto/ParkingSlot.java
+++ src/main/java/com/crocopie/parking/dto/ParkingSlot.java
@@ -2,9 +2,11 @@ package com.crocopie.parking.dto;
 
 import com.crocopie.parking.enumerated.Size;
 import com.crocopie.parking.enumerated.SlotStatus;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ParkingSlot {
 
     private int id;
--- src/main/java/com/crocopie/parking/enumerated/Size.java
+++ src/main/java/com/crocopie/parking/enumerated/Size.java
@@ -1,5 +1,14 @@
 package com.crocopie.parking.enumerated;
 
+import lombok.Getter;
+
+@Getter
 public enum Size {
-    S, M, L
+    S(1), M(2), L(4);
+
+    private final int capacity;
+
+    Size(int capacity) {
+        this.capacity = capacity;
+    }
 }
